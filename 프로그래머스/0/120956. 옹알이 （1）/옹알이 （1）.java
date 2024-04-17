8d09c5fee57f3709a38b9b1271a41e092532f514
Firemanhyeon <134985186+Firemanhyeon@users.noreply.github.com>
2024-04-17 09:11:36 +0900

    [level 0] Title: 옹알이 (1), Time: 0.71 ms, Memory: 75.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.java
@@ -0,0 +1,18 @@
+import java.util.List;
+import java.util.ArrayList;
+class Solution {
+    public int solution(String[] babbling) {
+        int answer = 0;
+        List<String[]> list = new ArrayList<>();
+        for(int i=0;i<babbling.length;i++){
+            String[] tmp = babbling[i].split("aya|ye|woo|ma");
+            list.add(tmp);
+        }
+        for(int i=0;i<list.size();i++){
+            if(list.get(i).length==0){
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
