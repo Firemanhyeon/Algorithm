e2efd3af0b0eb873b0edb206088061829a5aa2d5
Firemanhyeon <134985186+Firemanhyeon@users.noreply.github.com>
2024-04-19 21:12:21 +0900

    [level 1] Title: 음양 더하기, Time: 0.07 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
@@ -2,14 +2,13 @@ class Solution {
     public int solution(int[] absolutes, boolean[] signs) {
         int answer = 0;
         for(int i=0;i<absolutes.length;i++){
-            answer+=absolutes[i];
-        }
-        for(int i=0;i<signs.length;i++){
             if(!signs[i]){
-                answer-=absolutes[i]*2;
+                answer-=absolutes[i];
+            }else{
+                answer+=absolutes[i];
             }
+            
         }
-        System.out.println(answer);
         return answer;
     }
 }
