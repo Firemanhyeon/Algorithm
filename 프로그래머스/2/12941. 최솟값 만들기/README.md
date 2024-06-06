a13277ff4968ab071cd111bc960c608cbb0df5b9
Firemanhyeon <134985186+Firemanhyeon@users.noreply.github.com>
2024-06-07 00:28:32 +0900

    [level 2] Title: 최솟값 만들기, Time: 4.87 ms, Memory: 52.4 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/README.md
+++ 프로그래머스/2/12941. 최솟값 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 최솟값 만들기 - 12941 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12941#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12941) 
 
 ### 성능 요약
 
-메모리: 53 MB, 시간: 2.68 ms
+메모리: 52.4 MB, 시간: 4.87 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 07일 00:22:39
+2024년 06월 07일 00:28:30
 
 ### 문제 설명
 
--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
@@ -1,14 +1,17 @@
 import java.util.*;
+import java.util.stream.*;
 class Solution{
     public int solution(int []A, int []B){
         int answer = 0;
         Arrays.sort(A);
         Arrays.sort(B);
-        int idx=B.length-1;
-        for(int i=0;i<A.length;i++){
-            answer+= A[i]*B[idx];
-            idx--;
-        }
+        // int idx=B.length-1;
+        // for(int i=0;i<A.length;i++){
+        //     answer+= A[i]*B[idx];
+        //     idx--;
+        // }
+        
+        answer = IntStream.range(0,A.length).map(i-> A[i]*B[B.length-1-i]).sum();
         return answer;
     }
 }
