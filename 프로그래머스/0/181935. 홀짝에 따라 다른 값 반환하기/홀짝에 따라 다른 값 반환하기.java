8ad79a281132f920222a3e8d9d01a1504b93160a
Firemanhyeon <134985186+Firemanhyeon@users.noreply.github.com>
2024-06-18 19:19:36 +0900

    [level 0] Title: 홀짝에 따라 다른 값 반환하기, Time: 1.22 ms, Memory: 72.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.java
@@ -0,0 +1,7 @@
+import java.util.stream.*;
+
+class Solution {
+    public int solution(int n) {
+        return n%2==1 ? IntStream.rangeClosed(1,n).filter(i-> i%2==1).sum() : IntStream.rangeClosed(1,n).filter(i-> i%2==0).map(i->i*i).sum() ;
+    }
+}
