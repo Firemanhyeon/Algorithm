b2ca791a96c802e9c63ecbacc155b4d311816973
Firemanhyeon <ghgus990129@naver.com>
2024-11-20 18:26:50 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.07 ms, Memory: 52.2 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.java
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.java
@@ -1,25 +1,26 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        
-        int oneSize = getLength(n);
-        for(int i=n+1;i<=1000000;i++){
-            int getOne = getLength(i);
-            if(oneSize==getOne){
-                answer = i;
+        String str = Integer.toBinaryString(n);
+        int len =0;
+        for(int i=0;i<str.length();i++){
+            if(str.charAt(i)=='1'){
+                len++;
+            }
+        }
+        for(int i=n+1;i<1000000;i++){
+            String str2 = Integer.toBinaryString(i);
+            int len2=0;
+            for(int j=0;j<str2.length();j++){
+                if(str2.charAt(j)=='1'){
+                    len2++;
+                }
+            }
+            if(len==len2){
+                answer=i;
                 break;
             }
         }
         return answer;
-    }
-    public int getLength(int num){
-        StringBuilder str = new StringBuilder();
-        String len = Integer.toBinaryString(num);
-        for(int i=0;i<len.length();i++){
-            if(len.charAt(i)=='1'){
-                str.append(len.charAt(i));
-            }
-        }
-        return str.length();
     }
 }
