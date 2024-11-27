527963b1d41908ec234a7ea3e70d49c2f0be26e0
Firemanhyeon <ghgus990129@naver.com>
2024-11-27 20:40:04 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.03 ms, Memory: 54.8 MB -BaekjoonHub

--- 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.java
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.java
@@ -2,15 +2,16 @@ import java.util.*;
 
 public class Solution {
     public int solution(int n) {
-        int cnt =1;
-        while(n/2>0){
-            if(n%2==1){
-                cnt++;
+
+        int count=0;
+        while(n>0){
+            if(n%2!=0){
+                n--;
+                count++;
             }
             n=n/2;
         }
         
-        
-        return cnt;
+        return count;
     }
 }
