040217e80ea28ab003cb2d1162b54bd7df4ee045
Firemanhyeon <ghgus990129@naver.com>
2024-11-28 21:01:00 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.23 ms, Memory: 84.2 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -3,24 +3,34 @@ import java.util.*;
 class Solution {
     public int[] solution(int n, String[] words) {
         int[] answer = new int[2];
+        Map<Integer,Integer> map = new HashMap<>();
+        Set<String> set = new HashSet<>();
 
-        List<String> list = new ArrayList<>();
-        list.add(words[0]);
-        for(int i=1;i<words.length;i++){
-                //끝자리가 같지않은경우
-                if(!list.get(i-1).endsWith(words[i].charAt(0)+"")){
-                    answer[0]=list.size()%n+1;
-                    answer[1]=list.size()/n+1;
-                    break;
- 
-                //같은 단어 말했을때
-                }if(list.contains(words[i])){
-                   answer[0]=list.size()%n+1;
-                    answer[1]=list.size()/n+1;
-                    break;
-                }
-            
-            list.add(words[i]);
+        for(int i=1;i<=n;i++){
+            map.put(i,0);
+        }
+
+        char end = words[0].charAt(words[0].length()-1);
+
+        for(int i=0;i<words.length;i++){
+            char start = words[i].charAt(0);
+            //겹치는 단어를 말했을때
+            if(set.contains(words[i])){
+                answer[0] = (i%n)+1;
+                answer[1] = (i/n)+1;
+                break;
+
+            //끝자리 단어와 일치하지 않을때
+            }else if(start != end && i!=0){
+                answer[0] = (i%n)+1;
+                answer[1] = (i/n)+1;
+                break;
+            //정상패스
+            }else{
+                set.add(words[i]);
+                map.put(i%n+1,map.getOrDefault(i%n+1,0)+1);
+                end = words[i].charAt(words[i].length()-1);
+            }
         }
         return answer;
     }
