514d92b60c2a8318e8f8b25322dda2c1818c5457
Firemanhyeon <ghgus990129@naver.com>
2024-12-02 22:21:49 +0900

    [level 2] Title: 귤 고르기, Time: 130.89 ms, Memory: 114 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
@@ -1,18 +1,26 @@
 import java.util.*;
-import java.util.stream.*;
+
 class Solution {
     public int solution(int k, int[] tangerine) {
+        int answer = 0;
         Arrays.sort(tangerine);
         Map<Integer,Integer> map = new HashMap<>();
+        List<Integer> list = new ArrayList<>();
         for(int i=0;i<tangerine.length;i++){
-            map.put(tangerine[i],map.getOrDefault(tangerine[i] , 0)+1);
+            map.put(tangerine[i],map.getOrDefault(tangerine[i],0)+1);
         }
-        
-        int[] sortedArr = Arrays.stream(tangerine).boxed().sorted((a,b)-> map.get(b)-map.get(a)).mapToInt(i-> i).toArray();
-        Set<Integer> set = new HashSet<>();
-        for(int i=0;i<k;i++){
-            set.add(sortedArr[i]);
+        for(Map.Entry<Integer,Integer> entry : map.entrySet()){
+            list.add(entry.getValue());
         }
-        return set.size();
+        Collections.sort(list);
+        int sum = 0;
+        for(int i=list.size()-1;i>=0;i--){
+            answer++;
+            sum+=list.get(i);
+            if(sum>=k){
+                break;
+            }
+        }
+        return answer;
     }
 }
