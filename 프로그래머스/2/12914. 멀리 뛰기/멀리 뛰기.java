22bec967c95e9d126105c1c25ef86fd29744bca2
Firemanhyeon <ghgus990129@naver.com>
2024-12-06 02:35:32 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.18 ms, Memory: 83.5 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -1,14 +1,17 @@
 class Solution {
     public long solution(int n) {
         long answer = 0;
-        long a =0;
-        long b=1;
-        long result=0;
-        for(int i=1;i<=n;i++){
-            answer = (a+b)%1234567;
+        long a = 1;
+        long b= 2;
+        if(n==1) return a;
+        if(n==2) return b;
+        for(int i=3;i<=n;i++){
+            long cnt = (a+b)%1234567;
             a=b;
-            b=answer;// 1
+            b=cnt;
+            answer=cnt%1234567;
         }
-        return answer%1234567;
+        
+        return answer;
     }
 }
