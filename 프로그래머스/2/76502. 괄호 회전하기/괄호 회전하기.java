14230acabd9a05cdf3c2dbef258cc5aa15901513
Firemanhyeon <ghgus990129@naver.com>
2024-12-11 02:53:40 +0900

    [level 2] Title: 괄호 회전하기, Time: 46.46 ms, Memory: 94.8 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -1,40 +1,46 @@
 import java.util.*;
 
 class Solution {
-    public String swap(String s ){
-        String temp = s.substring(0,1);
-        return s.substring(1)+temp;
-    }
-    public boolean chk(String s){
-        Stack<Character> stack = new Stack<>();
-        
-        for (int i = 0; i < s.length(); i++) {
-            char current = s.charAt(i);
-            
-            if (current == '(' || current == '[' || current == '{') {
-                stack.push(current);
-            } else if (current == ')' && !stack.isEmpty() && stack.peek() == '(') {
-                stack.pop();
-            } else if (current == ']' && !stack.isEmpty() && stack.peek() == '[') {
-                stack.pop();
-            } else if (current == '}' && !stack.isEmpty() && stack.peek() == '{') {
-                stack.pop();
-            } else {
-                return false;
-            }
-        }
-        
-        return stack.isEmpty();
-    }
     public int solution(String s) {
         int answer = 0;
-        
+
         for(int i=0;i<s.length();i++){
-            s = swap(s);
             if(chk(s)){
                 answer++;
             }
+            s = next(s);
         }
         return answer;
+    }
+
+
+    //왼쪽으로 이동
+    public static String next(String s){
+        String str = s.substring(0,1);
+        return s.substring(1)+str;
+    }
+    //조건에 부합하는지 체크
+    public static boolean chk( String s ){
+        Stack<Character> stack = new Stack<>();
+        for(int i=0;i<s.length();i++){
+
+            char ch = s.charAt(i);
+            if(ch == '[' || ch == '{' || ch == '('){
+                stack.add(ch);
+            }else if(!stack.isEmpty() && ch == ']' && stack.peek() == '['){
+                stack.pop();
+            }else if(!stack.isEmpty() && ch == '}'&& stack.peek() == '{'){
+                stack.pop();
+            }else if(!stack.isEmpty() && ch == ')'&& stack.peek() == '('){
+                stack.pop();
+            }else{
+                return false;
+            }
+        }
+        if(stack.isEmpty()){
+            return true;
+        }
+        return false;
+
     }
 }
