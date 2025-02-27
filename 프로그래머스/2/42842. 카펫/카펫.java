c5680121316b910d2141771858bea2a0fd7e2475
Firemanhyeon <ghgus990129@naver.com>
2025-02-27 11:14:05 +0900

    [level 2] Title: 카펫, Time: 0.04 ms, Memory: 84.1 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,21 +1,17 @@
 class Solution {
     public int[] solution(int brown, int yellow) {
         int[] answer = new int[2];
-        
-        int total = brown+yellow;//총 칸 개수
-        
-        for(int i= total;i>0;i--){
-            if(total%i==0){
-                answer[0] = i;//가로 
-                answer[1] = total/i;//세로    
-              
-                if( (answer[0] -2) * (answer[1]-2) == yellow ){//brown 개수를 뺀 yellow 개수가 yellow랑 같을때 = 정답
-                    break;
+        int sum = brown+yellow;
+        for(int i=1;i<sum;i++){
+            if(sum % i == 0 ){
+                answer[1] = i;//세로
+                answer[0] = sum/i;//가로
+                
+                if( (answer[0] -2) * (answer[1]-2) == yellow ){
+                    return answer;
                 }
             }
         }
-        
-        
         return answer;
     }
 }
